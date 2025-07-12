9e7b96689ffb71bd8be7cd2e8d31a20c1bcaa3e6
helene <helene.maja@hotmail.dk>
2025-07-13 00:38:12 +0200

    add button to scr_2 and remove more unused code related to uart

--- src/main.c
+++ src/main.c
@@ -30,14 +30,8 @@ const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
 static const struct device *lvgl_encoder =
     DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_encoder_input));
 
-static uint8_t tx_buf[] = {"Software replacement for LCD\r\n"
-                           "Please type 'lime' or 'vodka'\r\n"};
-
-#define RECEIVE_BUFF_SIZE 10
-static uint8_t rx_buf[RECEIVE_BUFF_SIZE] = {0};
-#define RECEIVE_TIMEOUT 100
-static bool confirmed = false;
 enum states CURRENT_STATE = NORMAL;
+static bool drink_finished = false;
 
 enum drink_states
 {
@@ -55,17 +49,10 @@ static lv_obj_t *list1;
 static lv_obj_t *drink_status_label;
 
 // screens
+static lv_obj_t *scr_1;
 static lv_obj_t *scr_2;
 // progress bar
 static lv_obj_t *bar;
-
-typedef struct
-{
-    potion_recipes *user_recipe;
-    lv_event_t *user_event;
-} timer_user_data;
-
-static timer_user_data my_user_data;
 
 typedef void (*pre_mix_cb)(lv_event_t *e);
 typedef void (*mix_cb)(lv_event_t *e);
@@ -80,6 +67,14 @@ typedef struct
     uint8_t danger_level; // 0-255 (risk of disaster)
 
 } potion_recipes;
+
+typedef struct
+{
+    potion_recipes *user_recipe;
+    lv_event_t *user_event;
+} timer_user_data;
+
+static timer_user_data my_user_data;
 
 const char *get_drink_name(enum drinks d)
 {
@@ -100,7 +95,6 @@ const char *get_drink_name(enum drinks d)
         return NULL;
     }
 }
-
 
 void pump_ingredient(int ml, enum drinks drink)
 {
@@ -138,11 +132,9 @@ void pump_citrus()
     lv_label_set_text(drink_status_label, buffer);
 }
 
-
 void my_timer(lv_timer_t *timer)
 {
     /* Use the user_data */
-    static bool drink_finished = false;
     timer_user_data *user_data = lv_timer_get_user_data(timer);
     lv_event_t *event = user_data->user_event;
     printf("my_timer called\n");
@@ -250,13 +242,41 @@ void helper_print_state(enum states current_state)
     }
 }
 
+static void event_finish_handler(lv_event_t *e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    // lv_obj_t *obj = lv_event_get_target_obj(e);
+    // lv_obj_t *btn = lv_event_get_target(e);
+    if (code == LV_EVENT_PRESSED && drink_finished)
+    {
+        lv_scr_load(scr_1);
+        lv_bar_set_value(bar, 0, LV_ANIM_OFF);
+        drink_finished = false;
+    }
+}
+
 void create_new_screen()
 {
     scr_2 = lv_obj_create(NULL);
+
+    // Add a proper button (instead of making the screen clickable)
+    lv_obj_t *btn = lv_btn_create(scr_2);
+    lv_obj_set_size(btn, 35, 35);
+    lv_obj_t *btn_label = lv_label_create(btn);
+    lv_label_set_text(btn_label, LV_SYMBOL_LEFT);
+    lv_obj_center(btn_label);
+    lv_obj_align_to(btn, NULL, LV_ALIGN_TOP_LEFT, 20, 10);
+    lv_obj_set_style_bg_color(btn, lv_palette_main(LV_PALETTE_GREY), LV_STATE_DEFAULT);
+    // Add the button to the input group
+    lv_group_add_obj(g, btn);
+    // Add event to the button (not the screen)
+    lv_obj_add_event_cb(btn, event_finish_handler, LV_EVENT_PRESSED, NULL);
+
     drink_status_label = lv_label_create(scr_2);
     lv_obj_set_width(drink_status_label, 200);
     lv_obj_align(drink_status_label, LV_ALIGN_CENTER, 0, 0);
     lv_obj_set_style_text_font(drink_status_label, &lv_font_montserrat_24, 0);
+    // set button as back button from last message
 }
 
 void create_progress_bar()
@@ -292,15 +312,14 @@ void lv_make_drink_screen(lv_event_t *e)
     lv_obj_t *btn = lv_event_get_target(e);
     potion_recipes *data = lv_obj_get_user_data(btn);
     execute_one_recipe(data, e);
-    lv_scr_load(scr_2);
 }
 
 static void event_handler(lv_event_t *e)
 {
     lv_event_code_t code = lv_event_get_code(e);
     lv_obj_t *obj = lv_event_get_target_obj(e);
-    lv_obj_t *btn = lv_event_get_target(e);
-    if (code == LV_EVENT_PRESSED)
+    // lv_obj_t *btn = lv_event_get_target(e);
+    if (code == LV_EVENT_PRESSED && !drink_finished)
     {
         // LV_UNUSED(obj);
         LV_LOG_USER("Clicked: %s", lv_list_get_button_text(list1, obj));
@@ -309,16 +328,20 @@ static void event_handler(lv_event_t *e)
     }
 }
 
-void lv_example_list_1(potion_recipes *recipes, size_t recipes_size)
+void init_encoder()
 {
-
     g = lv_group_create();
     lv_group_set_default(g);
     lv_indev_t *encoder_indev = lvgl_input_get_indev(lvgl_encoder);
     lv_indev_set_group(encoder_indev, g);
 
+}
+
+void lv_example_list_1(potion_recipes *recipes, size_t recipes_size)
+{
     /*Create a list*/
-    list1 = lv_list_create(lv_screen_active());
+    scr_1 = lv_obj_create(NULL);
+    list1 = lv_list_create(scr_1);
     lv_group_add_obj(g, list1);
     lv_obj_set_size(list1, lv_pct(100), lv_pct(100));
     lv_obj_center(list1);
@@ -342,6 +365,7 @@ void lv_example_list_1(potion_recipes *recipes, size_t recipes_size)
         lv_obj_set_user_data(btn, &recipes[i]); // Explicitly set
         lv_obj_add_event_cb(btn, event_handler, LV_EVENT_PRESSED, NULL);
     }
+    lv_scr_load(scr_1);
 }
 
 int main(void)
@@ -363,15 +387,14 @@ int main(void)
     static potion_recipes recipes[] = {vodka_lime_recipe, gin_recipe, unknown_recipe};
     // table test
     size_t recipes_size = sizeof(recipes) / sizeof(recipes[0]);
+    init_encoder();
     create_new_screen();
     create_progress_bar();
     lv_example_list_1(recipes, recipes_size);
     lv_timer_handler();
     display_blanking_off(display_dev);
 
-
     helper_print_state(CURRENT_STATE);
-
 
     // if 'random' liquid has less than 'random' ml left in container
     // enter panic mode
